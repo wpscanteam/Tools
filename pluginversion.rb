aec4f69ba7707eee8ce161009365ae5f47ca4ef7
Peter <vanderlaan.pm@gmail.com>
2015-01-08 13:44:20 +0100

    Better 'readme' detection

--- pluginversion.rb
+++ pluginversion.rb
@@ -6,35 +6,45 @@ require 'net/http'
 
 def outputinfo(plugin)
 
-   url = "http://plugins.svn.wordpress.org/#{plugin}/trunk/readme.txt"
-   res = Net::HTTP.get_response(URI.parse(url.to_s))
+  # Possible readme filenames
+  readme = ["readme.txt", "README.txt", "README.TXT", "Readme.txt", "ReadMe.txt"]
+  i = 0
 
-   if res.code == '200'
-      f = open(url)
+  # Try to find a readme file
+  while i < readme.count do
+    url = "http://plugins.svn.wordpress.org/#{plugin}/trunk/#{readme[i]}"
+    res = Net::HTTP.get_response(URI.parse(url.to_s))
+    break if res.code == '200' # exit loop when a file is found
+    i += 1
+  end
 
-      version  = ""
-      tested   = ""
-      requires = ""
+  # Output data if a readme file is found.
+  if res.code == '200'
+    f = open(url)
 
-      regex1 = /[S|s]table tag: (.*)/
-      regex2 = /[T|t]ested up to: (.*)/
-      regex3 = /[R|r]equires at least: (.*)/
+    version  = ""
+    tested   = ""
+    requires = ""
 
-      f.each_line do |line|
-         version  = regex1.match(line)[1] if line =~ regex1
-         tested   = regex2.match(line)[1] if line =~ regex2
-         requires = regex3.match(line)[1] if line =~ regex3
-      end
+    regex1 = /[S|s]table tag: (.*)/
+    regex2 = /[T|t]ested up to: (.*)/
+    regex3 = /[R|r]equires at least: (.*)/
 
-      puts "URL         : #{url}"
-      puts "Plugin      : #{plugin}"
-      puts "Version     : #{version}"
-      puts "Requires WP : #{requires}"
-      puts "Tested up to: #{tested}"
-      puts "Last update : #{f.last_modified.strftime("%Y-%m-%d")}"
+    f.each_line do |line|
+      version  = regex1.match(line)[1] if line =~ regex1
+      tested   = regex2.match(line)[1] if line =~ regex2
+      requires = regex3.match(line)[1] if line =~ regex3
+    end
 
-   else
-      puts "No plugin found"
+    puts "URL         : #{url}"
+    puts "Plugin      : #{plugin}"
+    puts "Version     : #{version}"
+    puts "Requires WP : #{requires}"
+    puts "Tested up to: #{tested}"
+    puts "Last update : #{f.last_modified.strftime("%Y-%m-%d")}"
+
+  else
+    puts "No plugin found"
    end
 end
 
