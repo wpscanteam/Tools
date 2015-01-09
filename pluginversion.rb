f4fb1c173b91a167b8dd924ee89aa0689efe8740
erwanlr <erwan.lr@gmail.com>
2015-01-09 12:26:31 +0100

    Factorises things a bit

--- pluginversion.rb
+++ pluginversion.rb
@@ -4,53 +4,43 @@
 require 'open-uri'
 require 'net/http'
 
-def outputinfo(plugin)
-  # Get the correct readme filename
-  readme_file  = ''
-  regex_readme = /href=\"(.+)\"\>readme.txt/i
+plugin = ARGV[0]
 
-  url = "http://plugins.svn.wordpress.org/#{plugin}/trunk/"
-  res = Net::HTTP.get_response(URI.parse(url.to_s))
-
-  # Output data if a readme file is found.
-  if res.code == '200'
-    f = open(url)
-    f.each_line do |line|
-      readme_file = regex_readme.match(line)[1] if line =~ regex_readme
-    end
-
-    url = "http://plugins.svn.wordpress.org/#{plugin}/trunk/#{readme_file}"
-    # res = Net::HTTP.get_response(URI.parse(url.to_s))
-    f = open(url)
-
-    version  = ''
-    tested   = ''
-    requires = ''
-
-    regex1 = /stable tag:\s*(.+)/i
-    regex2 = /tested up to:\s*(.+)/i
-    regex3 = /requires at least:\s*(.+)/i
-
-    f.each_line do |line|
-      version  = regex1.match(line)[1] if line =~ regex1
-      tested   = regex2.match(line)[1] if line =~ regex2
-      requires = regex3.match(line)[1] if line =~ regex3
-    end
-
-    puts "URL         : #{url}"
-    puts "Plugin      : #{plugin}"
-    puts "Version     : #{version}"
-    puts "Requires WP : #{requires}"
-    puts "Tested up to: #{tested}"
-    puts "Last update : #{f.last_modified.strftime('%Y-%m-%d')}"
-
-  else
-    puts 'No plugin found'
-  end
+unless plugin
+  puts 'No argument supplied'
+  exit(1)
 end
 
-if ARGV[0]
-  outputinfo(ARGV[0])
+url = "http://plugins.svn.wordpress.org/#{plugin}/trunk/"
+res = Net::HTTP.get_response(URI.parse(url))
+
+# Output data if the trunk page is found.
+if res.code == '200'
+  readme_file = res.body[/href="(.+)">readme.txt/i, 1]
+
+  url = "http://plugins.svn.wordpress.org/#{plugin}/trunk/#{readme_file}"
+  f   = open(url)
+
+  version  = ''
+  tested   = ''
+  requires = ''
+
+  regex1 = /stable tag:\s*(.+)/i
+  regex2 = /tested up to:\s*(.+)/i
+  regex3 = /requires at least:\s*(.+)/i
+
+  f.each_line do |line|
+    version  = regex1.match(line)[1] if line =~ regex1
+    tested   = regex2.match(line)[1] if line =~ regex2
+    requires = regex3.match(line)[1] if line =~ regex3
+  end
+
+  puts "URL         : #{url}"
+  puts "Plugin      : #{plugin}"
+  puts "Version     : #{version}"
+  puts "Requires WP : #{requires}"
+  puts "Tested up to: #{tested}"
+  puts "Last update : #{f.last_modified.strftime('%Y-%m-%d')}"
 else
-  puts 'No argument supplied'
+  puts 'No plugin found'
 end
